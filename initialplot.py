eb586da4b62e159fa924939a6cc4854fa608f58a
emmahokken <emmahokken@gmail.com>
2019-06-03 20:13:17 +0200

    speed per hour plots

--- initialplot.py
+++ initialplot.py
@@ -1,26 +1,49 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime, timezone
+import numpy as np
+from scipy.signal import savgol_filter
 
 
-speed = []
-time = []
+speed16 = []
+speed19 = []
+time16 = []
+time19 = []
 
-format = "%Y-%m-%d %H:%M:%S %Z%z"
 
-
-with open('citea_e_20/speed.csv') as csv_file:
+with open('../citea_e_20/speed.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    init_day = 0
     for row in csv_reader:
         if line_count == 0:
             line_count += 1
             continue
-        else:
-            line_count += 1
-            speed.append(row[1])
-            time.append(datetime.utcfromtimestamp(int(row[0])/1000.))
+        elif line_count == 1:
+            init_day = datetime.utcfromtimestamp(int(row[0])/1000.)
+            init_day = init_day.day
+            print(datetime.utcfromtimestamp(int(row[0])/1000.))
+        line_count += 1
+        dateandtime = datetime.utcfromtimestamp(int(row[0])/1000.)
+        if dateandtime.day != init_day:
+            break
+        if dateandtime.hour < 16:
+            continue
+        elif dateandtime.hour == 16:
+            speed16.append(np.round(float(row[1]),2))
+            time16.append(dateandtime.minute)
+        elif dateandtime.hour == 17:
+            speed19.append(np.round(float(row[1]),2))
+            time19.append(dateandtime.minute)
 
-print(time[0], time[-1])
-plt.plot(speed, time)
+
+
+# print(time[/], time[-1])
+
+# smoothed = savgol_filter(speed, 1001, 3)
+# plt.plot(speed)
+plt.plot(time16, speed16)
+plt.title("speed between 16:00 and 17:00")
+plt.savefig('speed16.png')
+
 plt.show()
