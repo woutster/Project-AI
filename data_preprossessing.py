d0f7a7ceeb5077491b442cfc72b3ef5989de4fa6
emmahokken <emmahokken@gmail.com>
2019-06-11 16:23:45 +0200

    incorporated holidays plus 🐛 fixes

--- data_preprossessing.py
+++ data_preprossessing.py
@@ -3,6 +3,7 @@ import pandas as pd
 import datetime
 from datetime import datetime
 import numpy as np
+import requests
 
 def csv_to_pd(filename):
     """ Read in csv file with geofenced data and return dataframe. """
@@ -64,11 +65,14 @@ def match_weather_with_geo(geo, weather):
     # print(geo['temp'])
 
 def pd_to_csv(filename, geo, weather, one_hot_days):
-    # df.to_csv('combined.csv')
+
+    # Sort weather df.
+    weather = weather.sort_values(by='date')
+    weather.reset_index()
 
     with open(filename, mode='w') as csv_file:
         fieldnames = ['in_lat', 'in_long', 'in_time', 'out_lat', 'out_long', 'out_time',
-        'date', 'day', 'one_hot_day', 'time', 'temp', 'precipitation_intensity', 'wind_speed',
+        'date', 'day', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'holiday', 'time', 'temp', 'precipitation_intensity', 'wind_speed',
         'visibility']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -76,13 +80,15 @@ def pd_to_csv(filename, geo, weather, one_hot_days):
         for i, g in geo.iterrows():
 
             # Find proper weather data
-            temp, perc, wind, vis = 0, 0, 0, 0
+            temp, perc, wind, vis, time = 0, 0, 0, 0, 0
             for j, w in weather.iterrows():
-                if g['in_time'] == w['time'] or g['out_time'] == w['time'] :
+                if g['in_time'] <= w['time'] or g['out_time'] <= w['time'] :
                     temp, perc, wind, vis = w['temp'], w['precipitation_intensity'], w['wind_speed'], w['visibility']
+                    time = w['time']
                     break
                 elif g['date'] == w['date']:
                     temp, perc, wind, vis = w['temp'], w['precipitation_intensity'], w['wind_speed'], w['visibility']
+                    time = w['time']
                     break
 
             # Create one hot vector of days
@@ -90,12 +96,63 @@ def pd_to_csv(filename, geo, weather, one_hot_days):
 
             writer.writerow({'in_lat': g['in_lat'], 'in_long': g['in_long'], 'in_time': g['in_time'], 'out_lat':
             g['out_lat'], 'out_long': g['out_long'], 'out_time': g['out_time'],
-            'date': g['date'], 'day': g['day'], 'one_hot_day': one_hot_day_vec, 'time': weather['time'].iloc[i], 'temp': temp, 'precipitation_intensity': perc,
+            'date': g['date'], 'day': g['day'], 'monday': one_hot_days[0].iloc[i], 'tuesday': one_hot_days[1].iloc[i], 'wednesday': one_hot_days[2].iloc[i],
+            'thursday': one_hot_days[3].iloc[i], 'friday': one_hot_days[4].iloc[i], 'saturday': one_hot_days[5].iloc[i], 'sunday': one_hot_days[6].iloc[i],
+            'holiday': g['holiday'], 'time': time, 'temp': temp, 'precipitation_intensity': perc,
             'wind_speed': wind, 'visibility': vis})
 
 
-geo, one_hot_days = csv_to_pd('Data/proov_001/proov_001_geoFenced.csv')
-weather = weather_csv_to_pd('Data/proov_001/weather.csv')
+def holidays(geo):
+    base_url = 'https://calendarific.com/api/v2/holidays?'
+
+    api_key = '12bc7e5bac025c16e1daf74e12f8811c8460aec5'
+    year = '2018'
+    country = 'NL'
+    url18 = f'{base_url}&api_key={api_key}&country={country}&year={year}'
+    url17 = f'{base_url}&api_key={api_key}&country={country}&year=2017'
+
+    response_2018 = requests.get(url18)
+    response_2017 = requests.get(url17)
+    data18 = response_2018.json()
+    data17 = response_2017.json()
+
+    geo['holiday'] = 0
+
+    holidates = []
+    for hol in data18['response']['holidays']:
+        if int(hol['date']['datetime']['day']) < 10:
+            hol['date']['datetime']['day'] = '0' + str(hol['date']['datetime']['day'])
+        if int(hol['date']['datetime']['month']) < 10:
+            hol['date']['datetime']['month'] = '0' + str(hol['date']['datetime']['month'])
+
+        date = f"{hol['date']['datetime']['year']}-{hol['date']['datetime']['month']}-{hol['date']['datetime']['day']}"
+        holidates.append(date)
+    for hol in data17['response']['holidays']:
+        if int(hol['date']['datetime']['day']) < 10:
+            hol['date']['datetime']['day'] = '0' + str(hol['date']['datetime']['day'])
+        if int(hol['date']['datetime']['month']) < 10:
+            hol['date']['datetime']['month'] = '0' + str(hol['date']['datetime']['month'])
+
+        date = f"{hol['date']['datetime']['year']}-{hol['date']['datetime']['month']}-{hol['date']['datetime']['day']}"
+        holidates.append(date)
+
+    holiday_boolean_list = []
+    for i, g in geo.iterrows():
+        if str(g['date']) in holidates:
+            holiday_boolean_list.append(1)
+        else:
+            holiday_boolean_list.append(0)
+
+
+    geo['holiday'] = holiday_boolean_list
+
+    return geo
+
+
+bus = 'proov_002'
+geo, one_hot_days = csv_to_pd(f'Data/{bus}/{bus}_geoFenced.csv')
+weather = weather_csv_to_pd(f'Data/{bus}/weather.csv')
 
 # match_weather_with_geo(geo, weather)
-pd_to_csv('Data/proov_001/combined.csv', geo, weather, one_hot_days)
+geo = holidays(geo)
+pd_to_csv(f'Data/{bus}/combined.csv', geo, weather, one_hot_days)
